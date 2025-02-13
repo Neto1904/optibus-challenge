4cbd6ac19077aa90e69c6ca770e66a068654a088
Neto1904 <netohsa66@gmail.com>
2025-02-13 14:08:47 -0300

    Add duty_times_service module for calculating max and min time per duty

---
+++ src/modules/reports/services/duty_times_service.py
@@ -0,0 +1,34 @@
+
+from datetime import timedelta
+import pandas as pd  
+
+def parse_df_times(df):
+    parsed_df = df.copy()
+    # Split the 'start_time' and 'end_time' columns into 'start_day', 'start_time', 'end_day', and 'end_time'
+    parsed_df["start_day"] = parsed_df["start_time"].str.split(".", expand=True)[0]
+    parsed_df["end_day"] = parsed_df["end_time"].str.split(".", expand=True)[0]
+    parsed_df["start_time"] = parsed_df["start_time"].str.split(".", expand=True)[1]
+    parsed_df["end_time"] = parsed_df["end_time"].str.split(".", expand=True)[1]
+    # Calculate the number of days between the start and end time
+    parsed_df["days_of_difference"] = parsed_df["end_day"].astype(int) - parsed_df["start_day"].astype(int)
+    # Convert the 'start_time' and 'end_time' columns to datetime format
+    parsed_df["start_time"] = pd.to_datetime(parsed_df["start_time"])
+    parsed_df["end_time"] = pd.to_datetime(parsed_df["end_time"]) 
+    # Add the number of days to the 'end_time' column if the difference is positive
+    parsed_df["end_time"] = parsed_df.apply(lambda row: row['end_time'] + timedelta(days=row['days_of_difference']), axis=1)
+    return parsed_df
+
+def get_duty_times(df):
+    duty_vehicles_df = df.copy()
+    # Remove rows with missing values in the 'start_time' column
+    duty_vehicles_df = duty_vehicles_df.dropna(subset=['start_time'])
+    duty_vehicles_df = parse_df_times(duty_vehicles_df)
+    # Get the minimum and maximum time for each duty
+    min_max_time_df = duty_vehicles_df.groupby("duty_id", as_index=False).agg({
+        "start_time": "min", 
+        "end_time": "max",
+    })
+    # Convert the 'start_time' and 'end_time' columns to time format
+    min_max_time_df["start_time"] = pd.to_datetime(min_max_time_df["start_time"]).dt.time
+    min_max_time_df["end_time"] = pd.to_datetime(min_max_time_df["end_time"]).dt.time
+    return min_max_time_df
