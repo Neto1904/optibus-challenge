ca91ceadba15bc5aaa8ef71905119fccae0ad576
Neto1904 <netohsa66@gmail.com>
2025-02-14 09:53:16 -0300

    Refactor breaks_service.py for improved readability and efficiency

--- src/modules/reports/services/breaks_service.py
+++ src/modules/reports/services/breaks_service.py
@@ -4,59 +4,53 @@ from src.modules.reports.services.duty_times_service import parse_df_times
 from src.modules.reports.services.start_stop_description_service import get_stop_description
 
 def get_breaks(descriptions_df, vehicles_df, stops_df):
-    # create empty arrays to store the data that will be retrieved
-    break_duration_list = []
-    duty_ids_list = []
-    break_stops_name = [] 
-    break_start_times = []
+    break_duration_list, duty_ids_list, break_stops_name, break_start_times = [], [], [], []
+
     for index, row in descriptions_df.iterrows():
-        # filter by duty id and vehicle event type
-        filtered_df = vehicles_df[(vehicles_df["duty_id"] == row["duty_id"]) & (vehicles_df["vehicle_event_type"] == "deadhead")]
-        if len(filtered_df) == 0:
+        vehicles_df_filtered = filter_vehicles_df(vehicles_df, row["duty_id"])
+        if vehicles_df_filtered.empty:
             continue
-        parsed_df_times = parse_df_times(filtered_df)
-        # split the dataframe into equal parts of 2
+
+        parsed_df_times = parse_df_times(vehicles_df_filtered)
         splitted_dfs = split_dataframe(parsed_df_times, 2)
         break_durations, duty_ids, stop_ids, break_times = get_break_info(splitted_dfs)
-        stops_descriptions = []
-        # get the stop description for each stop id
-        for i, stop_id in enumerate(stop_ids):
-            stop_description = get_stop_description(stop_id, stops_df)
-            stops_descriptions.append(stop_description)
-        # append the data to the arrays
+        stops_descriptions = [get_stop_description(stop_id, stops_df) for stop_id in stop_ids]
+
         break_duration_list.extend(break_durations)
         duty_ids_list.extend(duty_ids)
         break_stops_name.extend(stops_descriptions)
         break_start_times.extend(break_times)
 
 
-    breaks_info = pd.DataFrame({
+    breaks_info = create_breaks_info_df(break_duration_list, duty_ids_list, break_stops_name, break_start_times)
+    joined_df = join_dataframes(breaks_info, descriptions_df, "duty_id")
+    return joined_df[["duty_id", "start_time", "end_time", "first_stop_description", "last_stop_description", "break_start_time","break_duration", "break_stops_name"]]
+
+def filter_vehicles_df(vehicles_df, duty_id):
+    return vehicles_df[(vehicles_df["duty_id"] == duty_id) & (vehicles_df["vehicle_event_type"] == "deadhead")]
+
+def create_breaks_info_df(break_duration_list, duty_ids_list, break_stops_name, break_start_times):
+    return pd.DataFrame({
         "break_duration": break_duration_list,
         "duty_id": duty_ids_list,
         "break_stops_name": break_stops_name,
         "break_start_time": break_start_times
     })
-    # join the dataframes to get the final table
-    joined_df = join_dataframes(breaks_info, descriptions_df, "duty_id")
-    return joined_df[["duty_id", "start_time", "end_time", "first_stop_description", "last_stop_description", "break_start_time","break_duration", "break_stops_name"]]
 
 def check_is_sequential(df):
     # check if the deadhead events are sequential
     first_event = df["vehicle_event_sequence"].astype(int).head(1).values[0]
     second_event = df["vehicle_event_sequence"].astype(int).tail(1).values[0]
-    if second_event - first_event == 1: 
-        return True
-    return False
+    return second_event - first_event == 1 
+       
 
 def get_break_info(dfs):
-    break_durations = []
-    duty_ids = []
-    stop_ids = []
-    break_times = []
-    for i, df in enumerate(dfs):
+    break_durations, duty_ids, stop_ids, break_times = [], [], [], []
+
+    for df in dfs:
         if not check_is_sequential(df): 
             continue
-        # get time difference and information associated with the break
+
         first_duty_end_time = df["end_time"].head(1).values[0]
         second_duty_start_time = df["start_time"].tail(1).values[0]
         duty_id = df["duty_id"].head(1).values[0]
