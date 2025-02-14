9793cb4eb144d6a10463f0a37c2a941556e8fbeb
Neto1904 <netohsa66@gmail.com>
2025-02-14 09:53:25 -0300

    Refactor duty_times_service.py for improved data parsing and readability

--- src/modules/reports/services/duty_times_service.py
+++ src/modules/reports/services/duty_times_service.py
@@ -1,19 +1,17 @@
 
 from datetime import timedelta
-import pandas as pd  
+import pandas as pd
 
 def parse_df_times(df):
     parsed_df = df.copy()
     # Split the 'start_time' and 'end_time' columns into 'start_day', 'start_time', 'end_day', and 'end_time'
-    parsed_df["start_day"] = parsed_df["start_time"].str.split(".", expand=True)[0]
-    parsed_df["end_day"] = parsed_df["end_time"].str.split(".", expand=True)[0]
-    parsed_df["start_time"] = parsed_df["start_time"].str.split(".", expand=True)[1]
-    parsed_df["end_time"] = parsed_df["end_time"].str.split(".", expand=True)[1]
+    parsed_df[["start_day", "start_time"]] = parsed_df["start_time"].str.split(".", expand=True)
+    parsed_df[["end_day", "end_time"]] = parsed_df["end_time"].str.split(".", expand=True)
     # Calculate the number of days between the start and end time
     parsed_df["days_of_difference"] = parsed_df["end_day"].astype(int) - parsed_df["start_day"].astype(int)
     # Convert the 'start_time' and 'end_time' columns to datetime format
     parsed_df["start_time"] = pd.to_datetime(parsed_df["start_time"])
-    parsed_df["end_time"] = pd.to_datetime(parsed_df["end_time"]) 
+    parsed_df["end_time"] = pd.to_datetime(parsed_df["end_time"])
     # Add the number of days to the 'end_time' column if the difference is positive
     parsed_df["end_time"] = parsed_df.apply(lambda row: row['end_time'] + timedelta(days=row['days_of_difference']), axis=1)
     return parsed_df
