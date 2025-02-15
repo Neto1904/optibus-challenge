c95a4d34d0dfe9e4b06c3ba60f8288f3f4275238
Neto1904 <netohsa66@gmail.com>
2025-02-15 15:31:33 -0300

    Add test case for no trips and no stops

--- tests/start_stop_description_test.py
+++ tests/start_stop_description_test.py
@@ -47,6 +47,74 @@ def test_get_start_stop_description():
 
     pd.testing.assert_frame_equal(result_df, expected_df)
 
+
+def test_get_start_stop_description_no_trips():
+    duty_times_data = {
+        "duty_id": [1, 2],
+    }
+    duty_times_df = pd.DataFrame(duty_times_data)
+    vehicles_data = {
+        "duty_id": [1, 1, 2, 2],
+        "vehicle_event_type": ["service_trip", "service_trip", "service_trip", "service_trip"],
+        "trip_id": [101, 102, 201, 202],
+    }
+    vehicles_df = pd.DataFrame(vehicles_data)
+    trips_data = {
+        "trip_id": [],
+        "origin_stop_id": [],
+        "destination_stop_id": [],
+    }
+    trips_df = pd.DataFrame(trips_data)
+    stops_data = {
+        "stop_id": [],
+        "stop_name": [],
+    }
+    stops_df = pd.DataFrame(stops_data)
+    expected_data = {
+        "duty_id": [1, 2],
+        "first_stop_description": ["No description", "No description"],
+        "last_stop_description": ["No description", "No description"],
+    }
+
+    expected_df = pd.DataFrame(expected_data)
+    result_df = get_start_stop_description(duty_times_df, trips_df, vehicles_df, stops_df)
+
+    pd.testing.assert_frame_equal(result_df, expected_df)
+
+def test_get_start_stop_description_no_stops():
+    duty_times_data = {
+        "duty_id": [1, 2],
+    }
+    duty_times_df = pd.DataFrame(duty_times_data)
+    vehicles_data = {
+        "duty_id": [1, 1, 2, 2],
+        "vehicle_event_type": ["service_trip", "service_trip", "service_trip", "service_trip"],
+        "trip_id": [101, 102, 201, 202],
+    }
+    vehicles_df = pd.DataFrame(vehicles_data)
+    trips_data = {
+        "trip_id": [101, 102, 201, 202],
+        "origin_stop_id": [1, 2, 3, 1],
+        "destination_stop_id": [2, 3, 1, 2],
+    }
+    trips_df = pd.DataFrame(trips_data)
+    stops_data = {
+        "stop_id": [],
+        "stop_name": [],
+    }
+    stops_df = pd.DataFrame(stops_data)
+    expected_data = {
+        "duty_id": [1, 2],
+        "first_stop_description": ["No description", "No description"],
+        "last_stop_description": ["No description", "No description"],
+    }
+
+    expected_df = pd.DataFrame(expected_data)
+    result_df = get_start_stop_description(duty_times_df, trips_df, vehicles_df, stops_df)
+
+    pd.testing.assert_frame_equal(result_df, expected_df)
+
+
 # Run the tests
 if __name__ == "__main__":
     pytest.main([__file__])
