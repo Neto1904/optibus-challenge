def92d54541e71fe654d8e5fad7eec052ebb9b1f
Neto1904 <netohsa66@gmail.com>
2025-02-15 15:59:42 -0300

    Remove unnecessary comments

--- src/modules/reports/services/start_stop_description_service.py
+++ src/modules/reports/services/start_stop_description_service.py
@@ -7,11 +7,9 @@ def get_start_stop_description(duty_times_df, trips_df, vehicles_df, stops_df):
         if filtered_vehicles_df.empty:
             continue     
         
-        # Get first and last trip id
         first_trip_id = filtered_vehicles_df.head(1)["trip_id"].values[0]
         last_trip_id = filtered_vehicles_df.tail(1)["trip_id"].values[0]
 
-        # Get first and last trip
         first_trip = trips_df[trips_df["trip_id"] == first_trip_id]
         last_trip = trips_df[trips_df["trip_id"] == last_trip_id]
 
@@ -22,21 +20,17 @@ def get_start_stop_description(duty_times_df, trips_df, vehicles_df, stops_df):
             last_stops_description.append('No description')
             continue
 
-        # Get first and last stop id
         first_stop_id = first_trip["origin_stop_id"].values[0]
         last_stop_id = last_trip["destination_stop_id"].values[0]
 
-        # Get first and last stop description
         first_stop_description = get_stop_description(first_stop_id, stops_df)
         last_stop_description = get_stop_description(last_stop_id, stops_df)
 
-        # Append to list
         first_stops_description.append(first_stop_description)
         last_stops_description.append(last_stop_description)
 
     descriptions_df = duty_times_df.copy()
     
-    # Add columns to dataframe
     descriptions_df["first_stop_description"] = first_stops_description
     descriptions_df["last_stop_description"] = last_stops_description
     return descriptions_df  
